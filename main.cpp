22967fb109641772b010c2bb0175f16fd417f240
hb <hb@gmail.com>
2018-03-26 14:53:55 +0530

    Initial release of web services

---
+++ main.cpp
@@ -0,0 +1,60 @@
+//
+//  Created by Ivan Mejia on 12/24/16.
+//
+// MIT License
+//
+// Copyright (c) 2016 ivmeroLabs.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#include <iostream>
+
+#include <usr_interrupt_handler.hpp>
+#include <runtime_utils.hpp>
+
+#include "microsvc_controller.hpp"
+
+using namespace web;
+using namespace cfx;
+
+void startMqttServer() {
+
+}
+
+void startServer() {
+	InterruptHandler::hookSIGINT();
+	MicroserviceController server;
+	server.setEndpoint("http://host_auto_ip4:6502/controlany/v1/api");
+	try {
+		server.accept().wait();
+		InterruptHandler::waitForUserInterrupt();
+		server.shutdown().wait();
+	} catch (std::exception & e) {
+		std::cerr << "somehitng wrong happen! :(" << '\n';
+	} catch (...) {
+		RuntimeUtils::printStackTrace();
+	}
+}
+
+int main(int argc, const char * argv[]) {
+	startMqttServer();
+	startServer();
+	return 0;
+}
